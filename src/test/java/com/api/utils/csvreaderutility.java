44b41bcf943f8dd7fb9842a0023f8b9d9774e0ef
Varsha Krishnakumar <Varsha8108@gmail.com>
2026-04-15 15:12:35 +0400

    created csv reader class to read the csv file using csvtobean class also created a bean class which acts as a data provider

---
+++ src/test/java/com/api/utils/csvreaderutility.java
@@ -0,0 +1,42 @@
+package com.api.utils;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+
+import com.demo.csv.pojouser;
+import com.opencsv.CSVReader;
+import com.opencsv.bean.CsvToBean;
+import com.opencsv.bean.CsvToBeanBuilder;
+
+public class csvreaderutility {
+
+	private csvreaderutility() {}
+	
+	public void loadcsvmethod(String path) {
+		
+InputStream is=Thread.currentThread().getContextClassLoader().getResourceAsStream(path);
+		
+		InputStreamReader isr=new InputStreamReader(is);
+		
+		CSVReader csvreader=new CSVReader(isr);
+		CsvToBean<pojouser> csvbean=new CsvToBeanBuilder(csvreader)
+				.withType(pojouser.class)
+				.withIgnoreEmptyLine(true)
+				.build();
+		
+		List<pojouser> pojouser=csvbean.parse();
+		System.out.println(pojouser);
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+}
