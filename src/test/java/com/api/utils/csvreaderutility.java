02772fd412ed3681b49dada0c143a9c82fa5a079
Varsha Krishnakumar <Varsha8108@gmail.com>
2026-04-15 15:16:22 +0400

    updated all the data provider and csv files to new location in src/test/java

--- src/test/java/com/api/utils/csvreaderutility.java
+++ src/test/java/com/api/utils/csvreaderutility.java
@@ -4,7 +4,7 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.List;
 
-import com.demo.csv.pojouser;
+import com.api.dataprovider.pojouser;
 import com.opencsv.CSVReader;
 import com.opencsv.bean.CsvToBean;
 import com.opencsv.bean.CsvToBeanBuilder;
